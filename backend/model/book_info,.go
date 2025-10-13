957d1a4d962385d6817c11ac8a883499cc86ad1f
powerangelXD <692732163@qq.com>
2025-10-13 17:26:06 +0800

    [Update] Update the models to adapt the dao

--- backend/model/book_info,.go
+++ backend/model/book_info,.go
@@ -1,17 +1,19 @@
 package model
 
-import "strconv"
+import (
+	"strconv"
+)
 
 type BookInfo struct {
-	Id       int    `json:"id"`
-	Title    string `json:"title"`
-	Author   string `json:"author"`
-	Rating   int    `json:"rating"`
-	CoverUrl string `json:"url"`
+	Id       int    `json:"id" gorm:"primaryKey;type:int:column:id"`
+	Title    string `json:"title" gorm:"not null;type:varchar(256);column:title"`
+	Author   string `json:"author" gorm:"not null;type:varchar(256);column:author"`
+	Rating   int    `json:"rating" gorm:"not null;type:int;column:rating"`
+	CoverUrl string `json:"url" gorm:"-"`
 }
 
 func (binfo *BookInfo) ToString() string {
-	return "{Book: \"" + strconv.Itoa(binfo.Id) + ": " + binfo.Title + "\", " +
+	return "{Book: \"" + strconv.Itoa(int(binfo.Id)) + ": " + binfo.Title + "\", " +
 		"Author: " + binfo.Author + ", " +
 		"Rating: " + strconv.Itoa(binfo.Rating) + "}"
 }
--- backend/model/user_info.go
+++ backend/model/user_info.go
@@ -1,9 +1,9 @@
 package model
 
 type UserInfo struct {
-	Id       int      `json:"id"`
-	Name     string   `json:"name"`
-	Birth    string   `json:"birth"`
-	Password string   `json:"password"`
-	Favs     []string `json:"favs"`
+	Id       int      `json:"id" gorm:"primaryKey;type:int;column:id"`
+	Name     string   `json:"name" gorm:"not null;type:varchar(100);column:name"`
+	Birth    string   `json:"birth" gorm:"not null;type:varchar(50);column:birth"`
+	Password string   `json:"password" gorm:"not null;type:varchar(20);column:password"`
+	Favs     []string `json:"favs" gorm:"type:text;serializer:json;column:favs"`
 }
