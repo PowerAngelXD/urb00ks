49dfb5312dc739d3da5aca85e2c729cceeba7ac0
powerangelXD <692732163@qq.com>
2025-10-16 00:50:41 +0800

    [Update] Update tests

--- backend/test/test_db.go
+++ backend/test/test_db.go
@@ -9,7 +9,7 @@ import (
 func TestDB() {
 	book := model.BookInfo{Title: "hello", Author: "Me", Rating: 7, ImageUrl: "http"}
 	dao.OfficialDB.Create(&book)
-	user := model.UserInfo{Name: "Liuj", Birth: "2020-03-04", Password: "hello-world"}
+	user := model.UserInfo{Name: "Liuji", Birth: "2020-03-04", Password: "hello-world"}
 	dao.OfficialDB.Create(&user)
 	logger.TestLog("Create bookinfo done!")
 }
