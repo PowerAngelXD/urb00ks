99983267e140eb6f8f92a6a63157f66e1931b061
powerangelXD <692732163@qq.com>
2025-12-01 15:38:37 +0800

    feat: Add the jwt auth system

---
+++ backend/middleware/jwt.go
@@ -0,0 +1,89 @@
+package middleware
+
+import (
+	"B00k/model"
+	"errors"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var Key = "SEVMTE9XT1JELU1PVEhFUkZVQ0tFUg=="
+
+type JWTStruct struct {
+	model.UserInfo
+	jwt.RegisteredClaims
+}
+
+func GenerateToken(user model.UserInfo) (string, error) {
+	claims := JWTStruct{
+		UserInfo: user,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			Issuer:    "urb00k-system",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(Key)
+}
+
+func ParseToken(token_str string) (*JWTStruct, error) {
+	token, err := jwt.ParseWithClaims(token_str, &JWTStruct{}, func(token *jwt.Token) (interface{}, error) {
+		return Key, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(*JWTStruct); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, errors.New("invalid token")
+}
+
+func JWTAuthHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHead := c.GetHeader("Authorization")
+		if authHead == "" {
+			c.JSON(http.StatusBadRequest,
+				model.ReturnStruct[int]{
+					Status: http.StatusBadRequest,
+					Msg:    "Auth failed",
+					Data:   1,
+				},
+			)
+		}
+
+		parts := strings.SplitN(authHead, " ", 2)
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.JSON(http.StatusBadRequest,
+				model.ReturnStruct[int]{
+					Status: http.StatusBadRequest,
+					Msg:    "Authorization header format must be Bearer {token}",
+					Data:   1,
+				},
+			)
+		}
+
+		claims, err := ParseToken(parts[1])
+		if err != nil {
+			c.JSON(http.StatusBadRequest,
+				model.ReturnStruct[int]{
+					Status: http.StatusBadRequest,
+					Msg:    err.Error(),
+					Data:   1,
+				},
+			)
+		}
+
+		c.Set("user", claims.UserInfo)
+		c.Next()
+	}
+}
