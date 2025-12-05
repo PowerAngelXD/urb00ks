2d1fefcc5664dc668ab61e025fcc6b7915a25db8
powerangelXD <692732163@qq.com>
2025-12-05 23:48:38 +0800

    feat: Add the router handler

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -16,6 +16,21 @@ import { createApp } from 'vue'
 // Styles
 import 'unfonts.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import axios from 'axios'
+
+// 请求拦截器
+axios.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem('auth_token');
+        if (token) {
+            config.headers['Authorization'] = 'Bearer ' + token;
+        }
+        return config;
+    }, 
+    error => {
+        return Promise.reject(error);
+    }
+);
 
 const app = createApp(App)
 
